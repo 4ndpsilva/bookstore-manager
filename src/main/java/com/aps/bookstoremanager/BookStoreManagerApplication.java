fc184ec12f702f37f763d7badff09aace56f9fbe
André <4ndpsilva@gmail.com>
2021-05-02 19:37:29 -0300

    First commit: project structure created

---
+++ src/main/java/com/aps/bookstoremanager/BookStoreManagerApplication.java
@@ -0,0 +1,11 @@
+package com.aps.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreManagerApplication.class, args);
+	}
+}
