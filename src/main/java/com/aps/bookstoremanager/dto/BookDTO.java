cbed60bbf52db950b39e63dc7dfead61d4cc6b7e
André P. Silva <andre.pereira@matera.com>
2021-08-11 18:16:31 -0300

    Adição de rotinas para tratamento de exceções

--- src/main/java/com/aps/bookstoremanager/dto/BookDTO.java
+++ src/main/java/com/aps/bookstoremanager/dto/BookDTO.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -30,8 +29,7 @@ public class BookDTO {
 
     @NotBlank
     @Size(max = 100)
-    @Pattern(regexp = "(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)",
-            message = "ISBN format must be a valid format")
+    //@Pattern(regexp = "(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)", message = "ISBN format must be a valid format")
     private String isbn;
 
     @NotBlank
---
+++ src/main/java/com/aps/bookstoremanager/exception/ApiExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.aps.bookstoremanager.exception;
+
+import com.aps.bookstoremanager.dto.ErrorDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.badRequest().body(getErrors(ex.getBindingResult(), status));
+    }
+
+    private List<ErrorDTO> getErrors(BindingResult bindingResult, HttpStatus httpStatus){
+        final List<ErrorDTO> errors = new ArrayList<>();
+        
+        bindingResult.getAllErrors().forEach(e -> {
+            final ErrorDTO error = new ErrorDTO();
+            error.setMessage(e.getDefaultMessage());
+            error.setStatusCode(httpStatus.value());
+            error.setTimestamp(LocalDateTime.now());
+            errors.add(error);
+        });
+
+        return errors;
+    }
+}
